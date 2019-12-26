36cf43ad5cb0123f262c5a5a3818219a113bde08
L.H <michael123303@yahoo.com>
2019-12-26 21:53:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,16 @@ LH SELF-BOT V1.0 2019/07/27
             V1.1 2019/08/02
             V1.2 2019/08/15
 '''
-import os
+import os, sys
+def run():
+     if sys.version_info[0] == 2:
+          print('This software does not support on python2.')
+          return
 try:
      os.system("python ./libs/main.py")
 except:
      pass
 exit()
+
+if __name__ == '__main__':
+    run()
